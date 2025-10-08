8dfb1d358a9f3422bbe4634b5c259a1f934e8e3a
amir <amir.kahrani7@gmail.com>
2025-10-08 17:02:58 +0330

    Build All Binding

--- src/api/handlers/Test_handler.go
+++ src/api/handlers/Test_handler.go
@@ -13,60 +13,171 @@ func NewTestHandler() *TestHandler {
 	return &TestHandler{}
 }
 
-func (h TestHandler) Test(c *gin.Context){
+func (h TestHandler) Test(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"result" : "ok",
+		"result": "ok",
 	})
 }
 
-
-
-
-	
-func (h TestHandler) Users(c *gin.Context){
+func (h TestHandler) Users(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"result" : "Users",
+		"result": "Users",
 	})
 }
 
-	
-func (h TestHandler) UserById(c *gin.Context){
+func (h TestHandler) UserById(c *gin.Context) {
 	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{
-		"result" : "UserById",
-		"id": id,
+		"result": "UserById",
+		"id":     id,
 	})
 }
 
-
-
-func (h TestHandler) UserByUsername(c *gin.Context){
+func (h TestHandler) UserByUsername(c *gin.Context) {
 	username := c.Param("username")
 	c.JSON(http.StatusOK, gin.H{
-		"result": "UserByUsername",
-		"username" : username,
-		
+		"result":   "UserByUsername",
+		"username": username,
 	})
 }
 
-
-func (h TestHandler) Accounts(c *gin.Context){
+func (h TestHandler) Accounts(c *gin.Context) {
 	id := c.Param("id")
 	c.JSON(http.StatusOK, gin.H{
 		"result": "Accounts",
-		"id" : id,
-		
+		"id":     id,
 	})
 }
 
-func (h TestHandler) AddUser(c *gin.Context){
-	id := c.Param("id")
+func (h TestHandler) AddUser(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"result": "AddUser",
-		"id" : id,
-		
+	})
+}
+
+// S23 CH3
+type header struct {
+	ApplicationType string
+	Browser          string
+}
+
+func (h *TestHandler) HeaderBinder1(c *gin.Context) {
+	userId := c.GetHeader("UserId")
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder1",
+		"userId": userId,
 	})
 }
 
 
 
+func (h *TestHandler) HeaderBinder2(c *gin.Context) {
+	obj := header{}
+	err := c.BindHeader(&obj)
+	if err  != nil{
+		c.JSON(http.StatusBadRequest, gin.H{
+		"result": "HeaderBinder1",
+		"err": err,
+	})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "HeaderBinder1",
+		"Header": obj,
+	})
+}
+
+
+func (h *TestHandler) QueryBinder1(c *gin.Context) {
+	id := c.Query("id")
+	name := c.Query("name")
+
+
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "QueryBinder1",
+		"name": name,
+		"id": id,
+
+	})
+}
+
+
+
+func (h *TestHandler) QueryBinder2(c *gin.Context) {
+	ids := c.QueryArray("id")
+	names := c.QueryArray("name")
+
+
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "QueryBinder2",
+		"names": names,
+		"ids": ids,
+
+	})
+}
+
+
+
+func (h *TestHandler) UriBinder(c *gin.Context) {
+	id := c.Param("id")
+	name := c.Param("name")
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "UriBinder",
+		"name": name,
+		"id": id,
+
+	})
+}
+
+type PersonData struct{
+	FirstName string
+	LastName string
+}
+
+
+func (h *TestHandler) BodyBinder(c *gin.Context) {
+	p := PersonData{}
+	// c.BindJSON(&p)
+	c.ShouldBindJSON(&p)
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "BodyBinder",
+		"Person": p,
+
+	})
+}
+
+func (h *TestHandler) FormBinder(c *gin.Context) {
+	p := PersonData{}
+	// c.BindJSON(&p)
+	c.Bind(&p)
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "FormBinder",
+		"Person": p,
+
+	})
+}
+
+
+
+func (h *TestHandler) FileBinder(c *gin.Context) {
+	file , _ := c.FormFile("myfile")
+	err := c.SaveUploadedFile(file, "file")
+	if err != nil{
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"result": "FileBinder",
+		"file": file.Filename,
+
+	})
+}
--- src/api/routers/test_router.go
+++ src/api/routers/test_router.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func TestRouter(r *gin.RouterGroup){
+func TestRouter(r *gin.RouterGroup) {
 	h := handlers.NewTestHandler()
 
 	r.GET("/", h.Test)
@@ -13,6 +13,20 @@ func TestRouter(r *gin.RouterGroup){
 	r.GET("/user/:id", h.UserById)
 	r.GET("/user/get-user-by-name/:username", h.UserByUsername)
 	r.GET("/user/:id/accounts", h.Accounts)
-	r.POST("/add-user", h.AddUser)
+	r.POST("/user/add-user", h.AddUser)
 
+	// 03
+	r.POST("/binder/header1", h.HeaderBinder1)
+	r.POST("/binder/header2", h.HeaderBinder2)
+
+	r.POST("/binder/query1", h.QueryBinder1)
+	r.POST("/binder/query2", h.QueryBinder2)
+	r.POST("/binder/uri1/:id/:name", h.UriBinder)
+
+	r.POST("/binder/body", h.BodyBinder)
+
+	r.POST("/binder/form", h.FormBinder)
+
+
+	r.POST("/binder/File", h.FileBinder)
 }
